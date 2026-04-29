3ba50743294fb2708d7f5a74e90679ac55f850c9
Niklas <menkniklas@gmail.com>
2026-04-29 23:30:04 +0200

    Added task 4 as pretty as possible.

---
+++ src/p4/aufgabe4.java
@@ -0,0 +1,38 @@
+package p4;
+
+public class aufgabe4 {
+    boolean hierEntstehtEinTollesUML_Diagram = false;
+    /*
+    * Enrollable
+    * <<Interface>>
+    *---------------
+    *---------------
+    * + enroll(student : Student) : void
+    *---------------
+    *   ^
+    *   |
+    *   |
+    *   |
+    * Course
+    * --------------
+    * - title : String
+    * - participant : Student
+    * --------------
+    * + Course(title : String, participant : Student)
+    * + getTitle() : String
+    *
+    *
+    *
+    * Student
+    * --------------
+    * - name: String
+    * - studentId : int
+    * --------------
+    * + Student(name : String, studentId : int)
+    * + getName() : String
+    * + enroll(student : Student) : void
+    *
+    *
+    *
+    * */
+}
